7ba45d594c6cbcac4aa29f33ec9be908cdffd947
hiragi-gkuth <hiragi.gkuth.dev@gmail.com>
2021-03-09 13:45:03 +0900

    A: ids model insert sqls

---
+++ vm/db/files/migration/create.sql
@@ -0,0 +1,9 @@
+CREATE TABLE uehara_2021021918 (
+`addr` VARCHAR(64) NOT NULL,
+`mask` INT unsigned NOT NULL,
+`entireperiod` INT unsigned NOT NULL,
+`div` INT unsigned NOT NULL,
+`pos` INT unsigned NOT NULL,
+`threshold` DOUBLE unsigned NOT NULL
+);
+
